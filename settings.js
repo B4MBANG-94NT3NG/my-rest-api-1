b4e0d0f8ed23cc8ef1497484f4d109c2e63eb227
B4MB4N9-ID <bambang@btr-store.my.id>
2023-12-25 20:27:21 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 
-global.creator = 'Zeeoneofc'// yourname
-global.MONGO_DB_URI = "" //database mongodb 
+global.creator = '@Someone.ex_'// yourname
+global.MONGO_DB_URI = "mongodb+srv://sadboy12:@Sadboy123@cluster0.kavmgfa.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
-global.email_password = "" //application password email
+global.your_email = "emailfresh3410987@gmail.com" //email
+global.email_password = "bulakamba" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
